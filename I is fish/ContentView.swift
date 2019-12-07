a2856e73b7ac64cef2df576867d00802966a5b92
Dlin2744 <w4559731234@gmail.com>
2019-12-07 13:35:28 +0800

    Initial Commit

---
+++ I is fish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I is fish
+//
+//  Created by User07 on 2019/12/7.
+//  Copyright © 2019 User07. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
